31d0f15c5671ae8982e4f3e48da28eb2a4b878dd
unknown <ibk9493@gmail.com>
2024-08-21 22:50:37 +0500

    23686

---
+++ JS/23686/a1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PhotoContestWidget from './components/PhotoContestWidget';
+
+function App() {
+  return (
+    <div className="App">
+      <PhotoContestWidget/>
+    </div>
+  );
+}
+
+export default App;
